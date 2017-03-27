2b091b1248ddec1a4457d40ade3fe6caa43d7e5c
Dana Weiss and Sowmya Dinavahi <dana@>
2017-03-27 13:04:28 -0700

    test commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,41 @@
 
 $(document).ready(function(){
-  $('form').submit(function(event){
-  event.preventDefault();
-  var blanks = ["name","color", "food", "pet", "friend", "vacation"];
-
-  var answerArr = [];
-  blanks.forEach(function(blank){
-    var userInput = $("input#" + blank).val();
-    answerArr.push(userInput);
 
 
-    $("." + blank).text(userInput);
-  });
+  // $('form').submit(function(event){
+  // event.preventDefault();
+  var flavors = ["vanilla","chocalate", "strawberry", "Butterscotch"];
 
-  newArray.push(answerArr[1]);
-  newArray.push(answerArr[2]);
-  newArray.push(answerArr[3]);
-  $("#answer-div").show();
 
-  var sites=["color-bullet", "food-bullet", "pet-bullet"]
-  var number = 0;
+  flavors.forEach(function(flavor){
+    var sites=["flavor1", "flavor2", "flavor3","flavor4"]
+    var number = 0;
 
-  sites.forEach(function(site){
-    $('.' + site).text(newArray[number]);
-    number++;
-  });
+    sites.forEach(function(site){
+      $('.' + site).text(flavor[number]);
+      number++;
+});
+
+
+
+  //   var userInput = $("input#" + flavor).val();
+  //   answerArr.push(userInput);
+  //
+  //
+  //   $("." + blank).text(userInput);
+  // });
+  //
+  // newArray.push(answerArr[1]);
+  // newArray.push(answerArr[2]);
+  // newArray.push(answerArr[3]);
+  // $("#answer-div").show();
+  //
+  // var sites=["color-bullet", "food-bullet", "pet-bullet"]
+  // var number = 0;
+  //
+  // sites.forEach(function(site){
+  //   $('.' + site).text(newArray[number]);
+  //   number++;
+
 });
 });
